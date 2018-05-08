65640a3a6f9b866f0c421ba975ec3b002475e3fd
npofsi <npofsi@outlook.com>
2018-05-08 21:45:31 +0800

    0.0.5-add beta.js(to build&run project quikly)

---
+++ beta.js
@@ -0,0 +1,45 @@
+PowerLauncherBetaTestRunning=(function(){
+var read=function(path){
+               try{
+                   var str="";
+                   var file=new java.io.File(path);
+                   var reader =  new  java.io.FileReader(file);
+                   var buffer =  java.lang.reflect.Array.newInstance(java.lang.Character.TYPE,file.length());
+                   var  charread =  0;
+                   reader.read(buffer)
+                   str=(new java.lang.String(buffer)).toString();
+                   reader.close();
+                   return str;
+               }catch(e){
+                   print(e.toString());
+               }
+           }
+"ui";
+var path=null;
+try{
+    path=""+libs_inthis.getIntent().getStringExtra("path");
+}catch(err){
+
+try{
+    path=""+engines.myEngine().getSource();
+}catch(err){
+
+}
+    //path=""//write your current abs path（no last '/')
+}
+//console.log(path)
+path=""+path
+path=path.replace(/\/beta.js$/,"")
+//print(path)
+var manifest=JSON.parse(""+read(path+"/package.json"));
+var code=read(path+"/loader/"+manifest.loader+"/"+manifest.loader+".dev.js");
+
+code=String(code
+.replace("{{PATH}}",""+path)
+.replace("\u200B","")).toString()
+//print(code)
+this.eval(code)
+return read(path+"/loader/"+manifest.loader+"/dist/output.bin.js")
+
+})
+eval(""+PowerLauncherBetaTestRunning())//
